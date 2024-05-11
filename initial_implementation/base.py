200392d232c129f63df7b6e173ae254e1d808a4d
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-05-11 21:49:43 +0300

    replace n_emb into emb_size

--- initial_implementation/base.py
+++ initial_implementation/base.py
@@ -109,7 +109,7 @@ class GPT(nn.Module):
        
         C.num_mlp=None
         C.block_size=None #None
-        C.model_type="gpt2"  #this is the equivalent of none for string present here 
+        C.model_type="gpt"  #this is the equivalent of none for string present here 
 
         return C
 
@@ -139,19 +139,19 @@ class GPT(nn.Module):
 
         if type_given:
             config.merge_from_dict({
-                'openai-gpt':   dict(n_layer=12, n_head=12, n_embd=768),  # 117M params
+                'openai-gpt':   dict(n_layer=12, n_head=12, emb_size=768),  # 117M params
                 # GPT-2 configs
-                'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-                'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
-                'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
-                'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+                'gpt2':         dict(n_layer=12, n_head=12, emb_size=768),  # 124M params
+                'gpt2-medium':  dict(n_layer=24, n_head=16, emb_size=1024), # 350M params
+                'gpt2-large':   dict(n_layer=36, n_head=20, emb_size=1280), # 774M params
+                'gpt2-xl':      dict(n_layer=48, n_head=25, emb_size=1600), # 1558M params
                 # Gophers
-                'gopher-44m':   dict(n_layer=8, n_head=16, n_embd=512),
+                'gopher-44m':   dict(n_layer=8, n_head=16, emb_size=512),
                 # (there are a number more...)
                 # I made these tiny models up
-                'gpt-mini':     dict(n_layer=6, n_head=6, n_embd=192),
-                'gpt-micro':    dict(n_layer=4, n_head=4, n_embd=128),
-                'gpt-nano':     dict(n_layer=3, n_head=3, n_embd=48),
+                'gpt-mini':     dict(n_layer=6, n_head=6, emb_size=192),
+                'gpt-micro':    dict(n_layer=4, n_head=4, emb_size=128),
+                'gpt-nano':     dict(n_layer=3, n_head=3, emb_size=48),
             }[config.model_type])
         
         if config.model_type is None:
